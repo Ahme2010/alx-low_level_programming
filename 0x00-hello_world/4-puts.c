8d702dded3522482064ecdb127f12fc725e45708
Ahme2010 <Ahme2010gc@gmail.com>
2022-06-30 14:21:18 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+* main -start point
+* Return: 0 if it is proper, othrwise non-zero
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
