1329d3c14ce0f893925b05deee37b54d5b4af768
Ahme2010 <Ahme2010gc@gmail.com>
2022-07-03 13:19:09 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,13 +18,11 @@ putchar((num2 / 10) + '0');
 putchar((num2 % 10) + '0');
 
 if (num1 == 98 && num2 == 99)
-    continue;
-
+continue;
 putchar(',');
 putchar(' ');
 }
 }
 putchar('\n');
-
 return (0);
 }
